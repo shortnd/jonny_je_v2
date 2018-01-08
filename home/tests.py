63bed02c51379e667b31b80027bbefd1c64ea824
shortnd <ococncol@gmail.com>
2018-01-08 12:23:13 -0500

    refactored home_page view to use a template

--- home/tests.py
+++ home/tests.py
@@ -7,6 +7,11 @@ from .views import home_page
 
 class HomePageTests(TestCase):
 
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+
+        self.assertTemplateUsed(response, 'home/home.html')
+
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
@@ -14,7 +19,7 @@ class HomePageTests(TestCase):
 
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>Home | JonnyJE Technical Support</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
 
     def test_home_page_displays_correct_h1_header(self):
         request = HttpRequest()
--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def home_page(request):
-    return HttpResponse('<html><head><title>Home | JonnyJE Technical Support</title></head><body><h1>Home Page JonnyJE Technical Support</h1></body></html>')
+    return render(request, 'home/home.html')
