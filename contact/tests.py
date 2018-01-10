0f17a393cab7c7870632832fda37b8ca17b83ccb
shortnd <ococncol@gmail.com>
2018-01-09 22:58:21 -0500

    added contact page a little bit more didn't write the correct test though

--- contact/tests.py
+++ contact/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from .views import contact_page
+
 
 class ContactPageTest(TestCase):
 
@@ -7,3 +9,19 @@ class ContactPageTest(TestCase):
         response = self.client.get('/contact/')
 
         self.assertTemplateUsed(response, 'contact/contact.html')
+
+    def test_contact_page_returns_correct_title_and_extends_base(self):
+        request = self.client.get('/contact/')
+        response = contact_page(request)
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>Contact | JonnyJE Technical Support</title>',
+                      html)
+
+    def test_h2_saying_contact_page(self):
+        request = self.client.get('/contact/')
+        response = contact_page(request)
+        html = response.content.decode('utf8')
+
+        self.assertIn('<h2>Contact Page</h2>', html)
