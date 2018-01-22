06d7940a7bf9d5795865526c98cc75ff102837ed
shortnd <ococncol@gmail.com>
2018-01-22 13:56:01 -0500

    added model for our database to save our sites

--- our_work/models.py
+++ our_work/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class OurWork(models.Model):
+    """A representation of the work that we have done for customers"""
+    title = models.CharField(max_length=200)
+    about_site = models.TextField()
+    imageURL = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.imageURL
