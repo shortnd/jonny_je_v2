c7e603fafc173ad6ac117c1edb3285317dd6c0d2
shortnd <ococncol@gmail.com>
2018-01-22 15:01:45 -0500

    changed urls to path because url is depreiciated

--- about_page/urls.py
+++ about_page/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'about_page'
 urlpatterns = [
-    url(r'^$', views.about_page, name='about_page'),
+    path('', views.about_page, name='about_page'),
 ]
