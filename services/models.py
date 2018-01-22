e3d58b8b82b302d8952d2f9a0e18c48609214285
shortnd <ococncol@gmail.com>
2018-01-22 14:33:28 -0500

    make services page display our services from the database

--- services/models.py
+++ services/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Services(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
--- services/views.py
+++ services/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Services
+
 
 def services(request):
-    return render(request, 'services/services.html')
+    services = Services.objects.all()
+    return render(request, 'services/services.html', {'services': services})
