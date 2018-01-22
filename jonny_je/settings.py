0c19c68fcbeebd402db02ffd25de71e077dbe426
shortnd <ococncol@gmail.com>
2018-01-22 13:29:28 -0500

    added settings for postgres

--- jonny_je/settings.py
+++ jonny_je/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'jonny_je.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jonnyjewebdevelopment',
+        'USER': 'shortnd',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
